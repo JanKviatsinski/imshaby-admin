ee6a4e234579735106f72ae2c3d247ff7f0dbb30
JanKviatsinski <yan5751839@gmail.com>
2021-07-05 17:47:17 +0300

    preEnd

--- src/components/sectionHeader/index.tsx
+++ src/components/sectionHeader/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { LogoutIcon } from '../icons';
 import './style.scss';
+import{ onChangeMode } from  '../../utils/colorScheme';
 
 interface props {
   title: string;
@@ -8,19 +9,20 @@ interface props {
   callback?: () => void;
 }
 
-const SectionHeader = ({ title, action = false, callback } : props) => (
+const SectionHeader = ({ title, action = false, callback, }: props) => (
   <section className="sectionHeader">
-    <span className="sectionHeader__title">{ title }</span>
+    <span className="sectionHeader__title">{title}</span>
     {
-        action && (
+      action && (
         <>
+          <button className="sectionHeader__action" onClick={onChangeMode}>Змянiць тэму</button>
           <button className="sectionHeader__action" onClick={callback}>
             <LogoutIcon className="sectionHeader__icon" />
             <span className="sectionHeader__txt">Выйсці</span>
           </button>
         </>
-        )
-      }
+      )
+    }
 
   </section>
 );
---
+++ src/utils/colorScheme.ts
@@ -0,0 +1,68 @@
+const currentTheme = localStorage.getItem('theme');
+
+if (currentTheme === 'dark') {
+  activateDarkMode()
+}
+
+function activateDarkMode() {
+  const rootElement: Element | null = document.querySelector(':root');
+  localStorage.setItem('theme', 'dark');
+  const darkTheme: {[key: string]: string} = {
+    '--main-text-color': '#F2F2F2',
+    '--second-text-color': '#B3B3B3',
+    '--main-icon-color': '#494949',
+    '--main-background': '#333333',
+    '--second-background': '#B3B3B3',
+    '--header-color': '#494949',
+    '--main-button-text-color': '#fff',
+    '--box-shadow': 'rgba(250, 250, 250, 0.247)',
+    '--normal-btn-background': '#B72025',
+    '--disabled-btn-background': '#666666',
+    '--hover-btn-background': '#a51015',
+    '--active-btn-background': '#6b1619',
+    '--second-color': 'rgba( 0, 0, 0, 0.3)',
+    '--main-btn': 'rgb(183, 32, 37)',
+    '--disabled-btn': 'rgba(183, 32, 37, .5)',
+    '--error': 'red',
+  };
+
+  for (let color in darkTheme) {
+    rootElement.style.setProperty(color, darkTheme[color])
+  }
+};
+
+function activateLightMode() {
+  const rootElement = document.querySelector(':root');
+  localStorage.setItem('theme', 'light');
+  const lightTheme: {[key: string]: string} = {
+    '--main-text-color': '#333333',
+    '--second-text-color': '#B3B3B3',
+    '--main-icon-color': '#494949',
+    '--main-background': '#F2F2F2',
+    '--second-background': '#fff',
+    '--header-color': '#333333',
+    '--main-button-text-color': '#fff',
+    '--box-shadow': 'rgba(183, 32, 37, 0.15)',
+    '--normal-btn-background': '#B72025',
+    '--disabled-btn-background': '#E98185',
+    '--hover-btn-background': '#D3040B',
+    '--active-btn-background': '#89181C',
+    '--second-color': 'rgba( 0, 0, 0, 0.3)',
+    '--main-btn': 'rgb(183, 32, 37)',
+    '--disabled-btn': 'rgba(183, 32, 37, .5)',
+    '--error': 'red',
+  };
+
+  for (let color in lightTheme) {
+    rootElement.style.setProperty(color, lightTheme[color])
+  }
+}
+
+export function onChangeMode() {
+  const currentTheme = localStorage.getItem('theme');
+  if (currentTheme === null || currentTheme === 'light') {
+    activateDarkMode()
+  } else {
+    activateLightMode()
+  }
+}
